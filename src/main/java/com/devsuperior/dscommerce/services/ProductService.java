c4ca69d8b16d976bc1562c930d83dc1ebb8f21c6
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-15 18:58:02 -0300

    Product GET findAll - pageable

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,8 +1,8 @@
 package com.devsuperior.dscommerce.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,9 +18,19 @@ public class ProductService {
 
     @Transactional(readOnly = true) // pra nao dar locking no banco de dados
     public ProductDTO findById(Long id) {
-        
-        Product product = repository.findById(id).get(); // retorna um Optional vazio se não existir o id passado, um optional de produto se encontrar
+
+        Product product = repository.findById(id).get(); // retorna um Optional vazio se não existir o id passado, um
+                                                         // optional de produto se encontrar
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    @Transactional(readOnly = true) // pra nao dar locking no banco de dados
+    public Page<ProductDTO> findAll(Pageable pageable) {
+
+        Page<Product> result = repository.findAll(pageable);
+        // return result.stream().map(x -> new ProductDTO(x)).toList(); // sem
+        // pageable...
+        return result.map(x -> new ProductDTO(x)); // map direto porq Page já é um stream do Java
     }
 }
