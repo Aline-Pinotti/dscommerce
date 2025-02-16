0c1ff079f39a7099cd790feba20a78b81d285956
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-16 09:07:00 -0300

    ResourceNotFoundExceprtion

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -19,8 +20,18 @@ public class ProductService {
     @Transactional(readOnly = true) // pra nao dar locking no banco de dados
     public ProductDTO findById(Long id) {
 
-        Product product = repository.findById(id).get(); // retorna um Optional vazio se não existir o id passado, um
-                                                         // optional de produto se encontrar
+        // retorna um Optional vazio se não existir o id passado, um Optional com o
+        // produto se existir
+        // Product product = repository.findById(id).get();
+
+        // utilizando o orElseThrow do Optional para lançar uma exceção caso o id não
+        // exista -
+        // *substitui o try catch*
+        // Product product = repository.findById(id).orElseThrow(() -> new
+        // IllegalArgumentException("Id not found")); //sem exception customizada
+        Product product = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado")); //interceptando a exceção do Optional e lançando a própria exceção
+                // importante lançar as próprias exceções e serviços para manter a arquitetura organizada, a camada de serviço é responsável pelas próprias exceções
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
