226968e51b2a4f83b3d35fee137c88883fe613d8
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-27 20:35:03 -0300

    UPDATE UserController getMe add PreAuthorize at route

--- src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -16,6 +17,7 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
     @GetMapping(value = "/me")
     public ResponseEntity<UserDTO> getMe() {
         UserDTO dto = service.getMe();
