71f3f227a89e734bde4e4518f4994c39d9e20ea4
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-28 06:16:45 -0300

    Category

--- src/main/java/com/devsuperior/dscommerce/services/AuthService.java
+++ src/main/java/com/devsuperior/dscommerce/services/AuthService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dscommerce.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.services.exceptions.ForbiddenException;
 
+@Service
 public class AuthService {
 
     @Autowired
